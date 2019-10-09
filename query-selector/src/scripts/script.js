800444446370d1c0727e638f3dea35d87eb3448c
Manila Bui <manila.bui@gmail.com>
2019-10-09 12:09:36 -0500

    completed exercise

--- query-selector/src/scripts/script.js
+++ query-selector/src/scripts/script.js
@@ -1,1 +1,9 @@
-console.log('hello beautiful')
+const firstHeaderEl = document.querySelector(".article__header");
+const headerEls = document.querySelectorAll(".article__header");
+const dashedEl = document.querySelector(".dashed");
+const footerEl = document.querySelector(".article__footer");
+
+firstHeaderEl.textContent = "Welcome to Manchilla's blog";
+headerEls.forEach(el => el.classList.add("important"));
+dashedEl.classList.remove("dashed");
+footerEl.classList.add("goldenrod");
