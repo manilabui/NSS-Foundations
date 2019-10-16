49dc9547bc51dbc3128f8ed29048ea4f9b904be0
Manila Bui <manila.bui@gmail.com>
2019-10-16 15:26:56 -0500

    scrolling and scaling exercise

--- js-events/src/scripts/main.js
+++ js-events/src/scripts/main.js
@@ -1,1 +1,23 @@
-console.log("Hello there, hot young thang.")
+console.log("Hello there, hot young thang.");
+
+// Mirror, Mirror in the Code
+const message = document.querySelector("#message");
+const mirrorLeft = document.querySelector("#mirror-left");
+const mirrorRight = document.querySelector("#mirror-right");
+
+const handleMessageKeyUp = e => {
+	mirrorLeft.textContent = e.target.value;
+	mirrorRight.textContent = e.target.value;
+};
+
+message.addEventListener("keyup", handleMessageKeyUp);
+
+
+// Scrolling and Scaling
+const audrey = document.getElementById("audrey");
+
+document.addEventListener("scroll", () => {
+   audrey.style.width = `${window.scrollY/3}px`;
+   audrey.style.height = `${window.scrollY/4}px`;
+})
+
