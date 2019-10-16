9836ad08763521c3accd75f4b73c9e46d441f5a8
Manila Bui <manila.bui@gmail.com>
2019-10-16 12:34:05 -0500

    added file structure and put input and boxes for mirrored text in DOM

--- daily-journal/src/scripts/journal.js
+++ daily-journal/src/scripts/journal.js
@@ -27,10 +27,10 @@ const makeJournalEntryComponent = entryObj => {
 
     return `
     	<section class=${id}>
-	    	<h2>${concept}<h2>
-	    	<h5>${date}<h5>
-	    	<h5>${mood}<h5>
-	    	<p>${entry}<p>
+	    	<h2>${concept}</h2>
+	    	<h5>${date}</h5>
+	    	<h5>${mood}</h5>
+	    	<p>${entry}</p>
 	    <section>
     `;
 };
