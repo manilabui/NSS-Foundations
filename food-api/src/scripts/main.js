ee8bd05e89e8dbb83eb04e09d5b75f5c98b9a1f6
Manila Bui <manila.bui@gmail.com>
2019-10-16 11:56:51 -0500

    finished food api fetching data exercise

--- food-api/src/scripts/main.js
+++ food-api/src/scripts/main.js
@@ -1,11 +1,14 @@
 const createFoodComponent = item => {
-	const { name, category, ethnicity, ingredients } = item;
+	const { name, category, ethnicity, ingredients, calories, fat, sugar } = item;
 
 	return `
 		<section class="food-item">
 			<h2>${name}</h2>
 			<h3>${ethnicity} ${category}</h3>
 			<p><strong>Ingredients:</strong> ${ingredients}</p>
+			<p><strong>Calories:</strong> ${calories}</p>
+			<p><strong>Fat:</strong> ${fat}</p>
+			<p><strong>Sugar:</strong> ${sugar}</p>
 		</section>	
 	`;
 };
@@ -17,10 +20,14 @@ const renderFoods = foods => {
 		fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
             .then(response => response.json())
             .then(foodInfo => {
-                food.ingredients = foodInfo.product.ingredients_text 
-                	? foodInfo.product.ingredients_text 
-                	: "no ingredients listed";
-                foodList.innerHTML += createFoodComponent(food);
+            	const { ingredients_text } = foodInfo.product;
+            	const { energy, fat, sugars, sugars_unit } = foodInfo.product.nutriments;
+
+                food.ingredients = ingredients_text ? ingredients_text : "no ingredients listed";
+                food.calories = energy ? energy : "calorie content unavailable";
+                food.fat = fat ? fat : "fat content unavailable";
+                food.sugar = sugars ? `${sugars}${sugars_unit}` : "sugar content unavailable";
+            	foodList.innerHTML += createFoodComponent(food);
             })
 	});
 };
