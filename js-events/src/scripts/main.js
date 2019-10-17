2148ca162ab2cfc26d3b8bc60a10e8770bc229c1
Manila Bui <manila.bui@gmail.com>
2019-10-16 23:56:55 -0500

    finished main exercises

--- js-events/src/scripts/main.js
+++ js-events/src/scripts/main.js
@@ -21,3 +21,21 @@ document.addEventListener("scroll", () => {
    audrey.style.height = `${window.scrollY/4}px`;
 })
 
+
+// Wonder Powers Activate!
+const flight = document.getElementById("flight");
+const mindreading = document.getElementById("mindreading");
+const xray = document.getElementById("xray");
+const allPowers = document.querySelectorAll(".power");
+
+const flightHandler = () => flight.className = "power enabled";
+const mindreadingHandler = () => mindreading.className = "power enabled";
+const xrayHandler = () => xray.className = "power enabled";
+const activateAllHandler = () => allPowers.forEach(power => power.className = "power enabled");
+const deactivateAllHandler = () => allPowers.forEach(power => power.className = "power disabled");
+
+document.querySelector("#activate-flight").addEventListener("click", flightHandler);
+document.querySelector("#activate-mindreading").addEventListener("click", mindreadingHandler);
+document.querySelector("#activate-xray").addEventListener("click", xrayHandler);
+document.querySelector("#activate-all").addEventListener("click", activateAllHandler);
+document.querySelector("#deactivate-all").addEventListener("click", deactivateAllHandler);
